de7f7e0f4f3675a075cb57337400847d8c321881
Jani <jani.alasirnio@gmail.com>
2024-10-28 17:08:16 +0200

    UT-1 4

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -17,7 +17,7 @@ class TestBowlingGame(unittest.TestCase):
         game = BowlingGame()
         self.assertRaises(BowlingError, game.get_frame_at,0)
 
-    def test_game_with_10_frames(self):
+    def test_game_with_11_frames(self):
        game = BowlingGame()
 
        game.add_frame(Frame(1,5))
@@ -31,5 +31,5 @@ class TestBowlingGame(unittest.TestCase):
        game.add_frame(Frame(8, 1))
        f = Frame(2, 6)
        game.add_frame(f)
-       self.assertEqual(f,game.get_frame_at(9))
+       self.assertRaises(BowlingError, game.add_frame, Frame(1, 1))
 
